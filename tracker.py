ac4e56e5ce1ae366c515af68609e88686ada2a81
Uchenna1-dev <maoforma50@gmail.com>
2025-11-10 22:37:28 +0000

    Progress: added add_expense and JSON saving logic

--- tracker.py
+++ tracker.py
@@ -1,34 +1,78 @@
+import json
+
+#create a list to store all expenses
+expenses =[
+    {"Groceries": [] },
+    {"Transport": [] },
+    {"Bills": [] },
+]
+
 # start with a welcome text 
 def main():
-    print("===================================")
-    print("   Welcome to your Expense Tracker  ")
-    print("===================================")
-    print("Let's start tracking your spending!\n")
+    while True:
 
-    print("Please choose an option: \n1. Add Expense \n2. View Expenses \n3. Exit")
+        print("===================================")
+        print("   Welcome to your Expense Tracker  ")
+        print("===================================")
+        print("Let's start tracking your spending!\n")
 
-    choice = int(input("Enter your choice: "))
+        print("Please choose an option: \n1. Add Expense \n2. View Expenses \n3. Exit")
 
-    while choice == 1:
-        # add expenses 
-        pass
+        choice = int(input("Enter your choice: "))
 
-    if choice == 2:
-        #view expenses
-        pass
+        if choice == 1:
+            add_expense()
+            print(expenses)
+    
 
-    elif choice == 3:
-        #exit
-        pass
+        if choice == 2:
+            #view expenses
+            pass
+
+        elif choice == 3:
+            break
+            
+            
+
+
+def add_expense():
+    #user to input expense
+    group = input("Category of expense: ")  #need to think of how to make it case sensitive
+    cost = float(input("Expense: "))
+    date = input("Date: ")
+    short_description = input("Short description: ")
+    match = False
+
+    for item in expenses:
+        for category, expense in item.items():
+            if group.lower() == category.lower():
+                expense.append({"cost": cost, "date": date, "description": short_description})
+                match = True
+
+    if match == False:
+        expenses.append({group: [{"cost": cost, "date": date, "description": short_description}]})
+
+ #print back to confirm expense added
+    print("Added")
+
+ #expense saved to memory 
+    with open("expenses.json","w") as file:
+        json.dump(expenses, file)
+#reload expenses file each time program runs, code defensively using try and except
+    try:
+        with open("expenses.json", "r") as file:
+        expenses = json.load(file)
+
+    except FIleNotFoundError:
+        expenses = []
+
+#should have a continue or go back to the main menu option
+    return
+
+
+    
 
 if __name__ == "__main__":
     main()
 
-def add_expenses():
-    #user to input expense
-    expense = float(input("Expense: "))
-    #expense saved to memory 
-  
-    #and added to list of expenses
-    #print back to confirm expense added
-    #should have a continue or go back to the main menu option
+
