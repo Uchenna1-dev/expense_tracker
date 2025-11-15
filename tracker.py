14a3a6b2556ef2ec42537f61d42668a8d20143dd
Uchenna1-dev <maoforma50@gmail.com>
2025-11-15 19:48:52 +0000

    Updated view expenses section, added option 2

--- tracker.py
+++ tracker.py
@@ -87,19 +87,64 @@ def add_expense():
 
 def view_expenses():
 
-    if not expenses:
-        print("No expenses found")
+    print("Please choose an option: \n1. View all expenses \n2. View expenses by categories \n3. View total spending \n4. Go back to main menu")
 
-    else:
-        for item in expenses:
-            for category, expense_details in item.items():
-                print(category)
-                print("----")
-                for expense in expense_details:
-                    print(expense["cost"], end= ' ')
-                    print ("on", end=' ')
-                    print(expense["date"], end= ' ')
-                    print(f"({expense['description']})")
+    option = int(input("Option: "))
+
+    if option == 1:
+
+        if not expenses:
+            print("No expenses found")
+
+        else:
+            for item in expenses:
+                for category, expense_details in item.items():
+                    print(category)
+                    print("----")
+                    for expense in expense_details:
+                        print(expense["cost"], end= ' ')
+                        print ("on", end=' ')
+                        print(expense["date"], end= ' ')
+                        print(f"({expense['description']})")
+
+        return
+
+#view expense by categories
+    elif option == 2:
+        if not expenses:
+            print("No expenses found under this category")
+            return
+        
+
+        chosen_category = input("Which category would you like to see: ").title()
+    
+
+        while chosen_category not in acceptable_categories:
+            print("Invalid category")
+            chosen_category = input("Which category would you like to see: ").title()
+
+        else:
+            for item in expenses:
+                
+                for category, expense_list in item.items():
+
+                    if category == chosen_category:
+                        print(category)
+
+                        if not expense_list:
+                            print("This category has no expense list")
+                            return
+                        
+                        for expense in expense_list:
+                            print(expense["cost"], end= ' ')
+                            print ("on", end=' ')
+                            print(expense["date"], end= ' ')
+                            print(f"({expense['description']})")
+
+            
+        
+
+    
 
 
     
